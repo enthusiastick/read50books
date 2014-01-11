4eb6bcaa6224e8561deb99922d777f47f236fe6f
Eben <eben.lowe@gmail.com>
2014-01-11 13:31:26 -0500

    Changed email configs for deployment to heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Read50books::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { :host => 'shielded-ocean-8498.herokuapp.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -8,17 +8,17 @@ if Rails.env.development?
 
 end
 
-# if Rails.env.production?
+if Rails.env.production?
 
-#   ActionMailer::Base.delivery_method = :smtp
-#   ActionMailer::Base.smtp_settings = {
-#     :address        => 'smtp.sendgrid.net',
-#     :port           => '587',
-#     :authentication => :plain,
-#     :user_name      => ENV['SENDGRID_USERNAME'],
-#     :password       => ENV['SENDGRID_PASSWORD'],
-#     :domain         => 'heroku.com',
-#     :enable_starttls_auto => true
-#   }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
-# end
+end
